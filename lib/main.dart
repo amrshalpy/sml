22fbea7b49c06317b063a49b382b7c54218641d1
amrshalpy <101294039+amrshalpy@users.noreply.github.com>
2022-07-02 20:09:42 +0200

    a

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:sportive/module/player/player_login/player_login.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
